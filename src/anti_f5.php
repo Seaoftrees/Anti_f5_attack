74ae997a1e8835d53c8fb4bebc38a7ae4edc8597
Seaoftrees <tasuzuya@gmail.com>
2017-04-05 00:19:35 +0900

    Add files via upload

---
+++ src/anti_f5.php
@@ -0,0 +1,58 @@
+<?php
+	function antiF5 ($view){
+
+		// Edit this ->
+
+		$src = "./thred/f5log.log";		//file directory
+		$msg = "Stopped load this web page a few seconds because of loading it too often.";	//message when f5 attack
+		$msg_color = "red";	//message color
+		$delay = 3; //delay time when load (sec)
+
+		// Edit this <-
+
+		echo "<!-- 'Anti f5 attack' was made by Seaoftrees. (MIT) -->\r\n";
+		$break = false;
+		$ipLong = decoct(ip2long($_SERVER["REMOTE_ADDR"])); 
+		$time = Time();
+		$original_data = "";
+
+		$fp = fopen($src, 'r');
+		if(flock($fp, LOCK_EX)){
+			$original_data = fgets($fp);
+			flock($fp, LOCK_UN);
+		}else{
+			echo "Failed to lock the file.";
+		}
+		fclose($fp);
+
+		$fp = fopen($src, 'w+');
+		if(flock($fp, LOCK_EX)){
+			if(strpos($original_data, "9") !== false){
+				$data = explode("9", $original_data);
+			}else{
+				$data = array(0 => "".$original_data);
+			}
+			$contents = "";
+			foreach ($data as $value) {
+				$del = false;
+				if($value){
+					List($ip, $past, $viewed) = explode("8", $value);
+					if(($ip===$ipLong)&&($viewed===$view)&&(($time-(octdec($past)))<$delay)){
+						$break = true;
+					}else{
+						if(($time-(octdec($past)))>($delay+10)) $del=true;
+					}
+				}
+				if(!$del) $contents = $contents.$ip."8".decoct($time)."8".$view;
+			}
+			fwrite($fp, $contents);
+			flock($fp, LOCK_UN);
+		}else{
+			echo "Failed to lock the file.";
+		}
+		fclose($fp);
+
+		if($break) exit('<font color="'.$msg_color.'">'.$msg.'</font>');
+
+	}
+?>
---
+++ src/index.php
@@ -0,0 +1,12 @@
+﻿<?php
+	require "./anti_f5-localhost.php";
+	antiF5("0");
+?>
+<!DOCTYPE html>
+<html>
+	<body>
+		<p>Please Repeated hits fast.</p>
+	</body>
+</html>
+
+
